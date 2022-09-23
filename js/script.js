40734b234151d6dbcec580fbe55d53eb3141fbb7
joohwho <jonathanhenriqueoliveira@gmail.com>
2022-09-22 23:18:03 -0300

    Criando js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const movie = {
+    title: "Karatê Kid",
+    url: "https://assets-global.website-files.com/60ff690cd7b0537edb99a29a/6255ef043ae3853f0d147635_karate-kid.png"
+};
+
+document.createElement()
